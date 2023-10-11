666080c220367ec46b599d72df99923b672ff841
kxhcfk <reveersed@mail.ru>
2023-10-11 17:10:37 +0300

    chore: webpack css, sass loaders

--- config/webpack/buildLoaders.ts
+++ config/webpack/buildLoaders.ts
@@ -1,17 +1,27 @@
 import {WebpackBuildOptions} from "./types/config";
 import webpack from "webpack";
 
-const buildLoaders = (options: WebpackBuildOptions): webpack.Configuration['module']['rules'] => {
+const buildLoaders = (options: WebpackBuildOptions): webpack.Configuration["module"]["rules"] => {
     const twigLoader = {
         test: /\.twig$/,
         use: {
-            loader: 'twig-loader',
-        }
+            loader: "twig-loader",
+        },
+    };
+    
+    const styleLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ],
     };
     
     return [
-        twigLoader
-    ]
-}
+        twigLoader,
+        styleLoader
+    ];
+};
 
-export {buildLoaders}
+export {buildLoaders};
